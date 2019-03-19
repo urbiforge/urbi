4caed17c424732de02e0403d97f5bc49c50be19b
Krzysztof Kubiak <k.kubiak@emys.co>
2019-03-19 19:37:34 +0000

    [CI] Automated builds

---
+++ misc/CI/prepare_boost.ps1
@@ -0,0 +1,12 @@
+$TARGETDIR = "$pwd\boost_bin"
+if(!(Test-Path -Path $TARGETDIR )){
+    echo "Building boost"
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+    iwr -outf boost.zip https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.zip
+    7z x -y boost.zip
+    cd boost_1_68_0
+    ./bootstrap.bat >$null
+    ./b2.exe -j4 --prefix=$pwd/../boost_bin --libdir=$pwd/../boost_bin/bin toolset=msvc link=shared address-model=64 install >$null
+	echo "Boost compiled and installed"
+    cd ..
+}
